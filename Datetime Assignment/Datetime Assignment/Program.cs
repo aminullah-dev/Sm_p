f7e073b9f91e3a4d8ed232d9976056090eabd843
aminullah-dev <Aminhashemi979@gmail.com>
2025-11-13 22:30:44 -0500

    Add DatetimeAssignment (DateTime and AddHours assignment with beginner comments)

---
+++ Datetime Assignment/Datetime Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;   // Allows use of DateTime and Console
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time
+            Console.WriteLine("Current Date and Time:");
+            Console.WriteLine(DateTime.Now);
+
+            // Ask the user to enter a number
+            Console.WriteLine("Enter a number of hours:");
+            int hours = Convert.ToInt32(Console.ReadLine());  // Convert user input to an integer
+
+            // Add the entered hours to the current time
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+
+            // Show the future time on the screen
+            Console.WriteLine("In " + hours + " hours, it will be:");
+            Console.WriteLine(futureTime);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
