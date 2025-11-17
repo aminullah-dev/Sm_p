f70cc388e76fc0d8a040105f16cc3353508c3702
aminullah-dev <Aminhashemi979@gmail.com>
2025-11-16 19:14:24 -0500

    Add ConstructorAssignment (const, var, constructor chaining)

---
+++ Constructor Assignment/Constructor Assignment/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    public class Person
+    {
+        // const variable - cannot change once set
+        public const string Country = "Canada";
+
+        // Class properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Constructor with one parameter
+        public Person(string name)
+        {
+            // Call the second constructor using a default age of 0
+            Name = name;
+            Age = 0;
+        }
+
+        // Constructor with two parameters
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Constructor Assignment/Constructor Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object using the constructor with one parameter
+            Person person1 = new Person("Amin");
+
+            // Create an object using the constructor with two parameters
+            Person person2 = new Person("Amin", 25);
+
+            // Use 'var' keyword to let C# decide the type automatically
+            var message = "Object created successfully";
+
+            // Print output
+            Console.WriteLine(message);
+            Console.WriteLine(person1.Name);
+            Console.WriteLine(person2.Name + " is " + person2.Age + " years old");
+
+            Console.ReadLine();
+        }
+    }
+}
